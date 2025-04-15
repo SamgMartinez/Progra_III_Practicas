b50b3e8827d357c123c6a818c1bdf10bb82b9539
Samuel <samg.martinez2@gmail.com>
2025-04-15 02:36:18 -0300

    Se creo y probo el MergeSorf

--- src/Order_methods/MergeSorf/MergeSorf.java
+++ src/Order_methods/MergeSorf/MergeSorf.java
@@ -3,17 +3,40 @@ package Order_methods.MergeSorf;
 import java.util.List;
 
 public class MergeSorf {
-    public List mergeSorf(List list, int init, int end){
+    public static int[] mergeSorf(int[] list, int init, int end){
+        if (init == end){
+            return list;
+        }
         int mid = (init + end)/2;
         if (init < end){
             mergeSorf(list, init, mid);
             mergeSorf(list, mid + 1, end);
         }
-
+        merge(list,init,end,mid);
         return list;
     }
 
-    private static List merge(List list, int init, int end, int mid){
+    private static int[] merge(int[] list, int init, int end, int mid){
+        int index1 = init, index2 = mid + 1;
+        int listaux[]  = new int[list.length];
+        for (int i = init ; i <= end; i++){
+            if (index1 > mid) {
+                listaux[i] = list[index2];
+                index2++;
+            }else if(index2 > end){
+                    listaux[i] = list[index1];
+                    index1++;
+            }else if(list[index1] < list[index2]){
+                listaux[i] = list[index1];
+                index1++;
+            }else {
+                listaux[i] = list[index2];
+                index2++;
+            }
+        }
+        for (int i = init ; i <= end; i++){
+            list[i] = listaux[i];
+        }
 
         return list;
     }
--- src/Order_methods/MergeSorf/_Main_.java
+++ src/Order_methods/MergeSorf/_Main_.java
@@ -1,32 +1,42 @@
 package Order_methods.MergeSorf;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class _Main_ {
-    private static List crearLista(int cantidad){
-        List<Integer> lista = new ArrayList<>();
+    private static int[] crearLista(int cantidad){
+        int lista[] = new int[cantidad];
         Random random = new Random();
 
-        for(int i=1; i<cantidad;i++){
+        for(int i=0; i<cantidad;i++){
             int randomInt = random.nextInt(100);
-            lista.add(randomInt);
+            lista[i] = randomInt;
         }
         return lista;
+    }
+    private static void mostrarLista(int[] list){
+        for(int i = 0; i < list.length; i++){
+            System.out.print("["+list[i]+"]");
+        }
+        System.out.print("\n");
     }
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        List<Integer> vectorDesordenado = crearLista(100);
-        int x = 3;
-        int y = 2;
-        System.out.println(x/y);
-
+        int vectorDesordenado[] = crearLista(500000);
+        //mostrarLista(vectorDesordenado);
+        int vectorOrdenador[] = MergeSorf.mergeSorf(vectorDesordenado, 0, vectorDesordenado.length - 1);
+        //mostrarLista(vectorOrdenador);
         //  Ini Area para medir el tiempo   //
         long endTime = System.currentTimeMillis();
         double elapsedTimeInSeconds = (endTime - startTime) / 1000.0;
         System.out.println("Execution time: " + elapsedTimeInSeconds + " seconds");
         //  End APMET    //
     }
+
+    //  Resultados de pruebas //
+    // cant = 100 -> 0.000 seconds
+    // cant = 1000 -> 0.000 seconds
+    // cant = 10000-> 0.101 seconds /3
+    // cant = 50000 -> 1.038 seconds /5
+    // cant = 100000 -> 3.001 seconds
+    // cant = 500000 -> sin respuesta
 }
